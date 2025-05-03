79d5c9858f6084fc409d16561885c7e0e4b93582
Baris Yildiz <yyildizbaris@gmail.com>
2025-05-03 12:39:49 +0300

    bug fix

--- init.lua
+++ init.lua
@@ -908,13 +908,9 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
-<<<<<<< Updated upstream
-=======
-
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
       vim.cmd.language 'en_US'
->>>>>>> Stashed changes
     end,
   },
 
